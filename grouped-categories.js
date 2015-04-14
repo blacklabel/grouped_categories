43896d26c3e19d152f675ca9a243720b2c88f0c8
Kody Smith <smithk@versifit.com>
2015-04-14 08:40:15 -0500

    Changed the look of grid lines

--- grouped-categories.js
+++ grouped-categories.js
@@ -198,7 +198,6 @@ axisProto.render = function () {
   // cache original tick length
   if (this.originalTickLength === UNDEFINED)
     this.originalTickLength = this.options.tickLength;
-
   // use default tickLength for not-grouped axis
   // and generate grid on grouped axes,
   // use tiny number to force highcharts to hide tick
@@ -211,7 +210,6 @@ axisProto.render = function () {
       this.labelsGrid.attr({visibility: 'hidden'});
     return;
   }
-  console.log(this);
   var axis    = this,
       options = axis.options,
       top     = axis.top,
@@ -224,10 +222,9 @@ axisProto.render = function () {
       horiz   = axis.horiz,
       d       = axis.labelsGridPath,
       i       = options.drawHorizontalBorders === false ? depth+1 : 0,
-      offset  = axis.opposite ? (horiz ? top : right) : (horiz ? bottom : left),
+	  offset  = axis.opposite ? (horiz ? top : right) : (horiz ? bottom+5 : left-5), //The additional 5 to offsets shifts the grids to prevent text from overlapping
       gridVisible = (!options.hideGrid && visible),
       part;
-      
   if (axis.userTickLength)
     depth -= 1;
 
@@ -241,19 +238,53 @@ axisProto.render = function () {
       })
       .add(axis.axisGroup);
   }
-
+  
+  //Calculate how mow much the base category is being multipled by
+  var currentTree = axis.categoriesTree;
+  var catMultiplier = 1;
+  for(var j=0; j < depth; j++){
+	  catMultiplier *= currentTree.length;
+	  currentTree = currentTree[0].categories;
+  }
+  //Calculate the amount of values in the first cat
+  var firstCatCount = axis.categories.length/catMultiplier;
+  //We increment through the grid array by <number of levels> * <amount of array indexes to define a point>(it takes 6 indexes in the array to define a line)
+  var arrayIncrement = (depth+1)*6;
+  //The X/Y position of the start of the grid (X pos if on Y axis, Y pos if on X axis)
+  var axisBaseYCoord = (horiz) ? d[2] : d[1];
+  //We target the X pos if on Y axis, we target the Y pos if on X axis)
+  var targetCoord = (horiz) ? 3 : 4;
+  //We check every 6th grid pos (but we skip the 1st one)
+  for(var j=11; j < d.length; j+=6){
+		//If the cur. position matches the end of a "set" of first categories, it's a divider we want to keep, so we skip the deletion
+		if((j+1)%(firstCatCount*arrayIncrement) == 0){
+			//We modify the current lines start position to be the grids base (covers the gap left by deleting all the 1st level lines)
+			d[j-targetCoord] = axisBaseYCoord;//or -3
+			//We skip the next grid line, since it's an extension of the current grid line
+			j+=6;
+			continue;
+		}
+		//Set line start position to 0
+		d[j-3] = 0;
+		d[j-4] = 0;
+		
+		//Set line end position to 0
+		d[j] = 0;
+		d[j-1] = 0;
+	  
+  }
+  
   // go through every level and draw horizontal grid line
   while (i <= depth) {
     offset += axis.groupSize(i);
 
     part = horiz ?
-      [left, offset, right, offset] :
-      [offset, top, offset, bottom];
+      [left, offset, right, offset] ://Draws line horizontally
+      [offset, top, offset, bottom]; //Draws line vertically, where 1st offset = offset of top from the chart, top = end point of top, bottom = end point of bottom, 2nd offset = offset of bottom from the chart 
 
-    addGridPart(d, part);
+    //addGridPart(d, part); Disabled this to remove horizontal lines. Uncomment to get them back.
     i++;
   }
-
   // draw grid path
   grid.attr({
     d: d,
@@ -345,13 +376,10 @@ axisProto.groupSize = function (level, position) {
 // Override methods prototypes
 tickProto.addLabel = function () {
   var category;
-
   _tickAddLabel.call(this);
-
   if (!this.axis.categories ||
       !(category = this.axis.categories[this.pos]))
     return;
-
   // set label text - but applied after formatter #46
   if (category.name && this.label)
     this.label.attr('text', this.axis.labelFormatter.call({
@@ -361,7 +389,7 @@ tickProto.addLabel = function () {
 			isLast: this.isLast,
 			value: category.name
 		}));
-
+  
   // create elements for parent categories
   if (this.axis.isGrouped && this.axis.options.labels.enabled)
     this.addGroupedLabels(category);
@@ -380,35 +408,41 @@ tickProto.addGroupedLabels = function (category) {
       size    = axis.horiz ? 'height' : 'width',
       depth   = 0,
       label;
-
-
+  
+  //Calculate the depth of the grouped categories, save it on the axis so it isn't done more than once
+  if(!axis.categoriesTotalDepth)
+	  axis.categoriesTotalDepth = calculateDepth(axis.categoriesTree);
   while (tick) {
     if (depth > 0 && !category.tick) {
       // render label element
       this.value = category.name;
-      var name = options.formatter ? options.formatter.call(this, category) : category.name,
+      var name = axis.labelFormatter ? axis.labelFormatter.call(this, category) : category.name,
       	  hasOptions = userAttr && userAttr[depth-1],
      	  mergedAttrs =  hasOptions ? merge(attr, userAttr[depth-1] ) : attr,
      	  mergedCSS = hasOptions && userAttr[depth-1].style ? merge(css, userAttr[depth-1].style ) : css;
-      label = chart.renderer.text(name, 0, 0, useHTML)
-        .attr(mergedAttrs)
-        .css(mergedCSS)
-        .add(axis.labelGroup);
-
+ 	label = chart.renderer.text(name, 0, 0, useHTML).attr(mergedAttrs).add(axis.labelGroup);
+ 	//Iterate down the grouped tree to count how many categories wide the current group is
+ 	var currentTree = axis.categoriesTree;
+ 	var categoryCount = 1;
+ 	for(var j=0; j <= (axis.categoriesTotalDepth-depth); j++){
+ 		categoryCount = categoryCount * currentTree.length;
+ 		currentTree = currentTree[0].categories;
+ 	}
+ 	mergedCSS.width = chart.plotWidth/categoryCount;
+ 	 label.css(mergedCSS);
       // tick properties
       tick.startAt = this.pos;
       tick.childCount = category.categories.length;
       tick.leaves = category.leaves;
       tick.visible = this.childCount;
       tick.label = label;
-
+      
       // link tick with category
       category.tick = tick;
     }
 
     // set level size
     axis.groupSize(depth, tick.label.getBBox()[size]);
-
     // go up to the parent category
     category = category.parent;
 
@@ -453,15 +487,12 @@ tickProto.render = function (index, old, opacity) {
       maxPos,
       attrs,
       bBox;
-
   // render grid for "normal" categories (first-level), render left grid line only for the first category
   if (isFirst) {
-    gridAttrs = horiz ?
-      [axis.left, xy.y, axis.left, xy.y + axis.groupSize(true)] :
-      axis.isXAxis ?
-        [xy.x, axis.top, xy.x + axis.groupSize(true), axis.top] :
-        [xy.x, axis.top + axis.len, xy.x + axis.groupSize(true), axis.top + axis.len];
-
+    gridAttrs = horiz ? [axis.left, xy.y, axis.left, xy.y + axis.groupSize(true)] :
+    	axis.isXAxis ? [xy.x, axis.top, xy.x + axis.groupSize(true), axis.top] :
+    		[xy.x, axis.top + axis.len, xy.x + axis.groupSize(true), axis.top + axis.len];
+    	
     addGridPart(grid, gridAttrs);
   }
 
@@ -499,18 +530,22 @@ tickProto.render = function (index, old, opacity) {
 			x: size + lvlSize / 2,
 			y: (minPos.y + maxPos.y - bBox.height) / 2  + baseLine
 		};
+	//Attempts to center the grouped labels a little more accurately
+	if(horiz)
+		attrs.y +=10;
+	else
+		attrs.x -=5;
+	if(!isNaN(attrs.x) && !isNaN(attrs.y)){ 
+		group.label.attr(attrs);
 
-		if(!isNaN(attrs.x) && !isNaN(attrs.y)){ 
-			group.label.attr(attrs);
-	
-			if (grid) {
-				if(horiz && axis.left < maxPos.x) {
-						addGridPart(grid, [maxPos.x, size, maxPos.x, size + lvlSize]);
-				} else if(!horiz && axis.top < maxPos.y){
-						addGridPart(grid, [size, maxPos.y, size + lvlSize, maxPos.y]);
-				}
+		if (grid) {
+			if(horiz && axis.left < maxPos.x) {
+					addGridPart(grid, [maxPos.x, size, maxPos.x, size + lvlSize]);
+			} else if(!horiz && axis.top < maxPos.y){
+					addGridPart(grid, [size, maxPos.y, size + lvlSize, maxPos.y]);
 			}
-    }
+		}
+	}
 
     size += lvlSize;
     depth++;
@@ -533,5 +568,9 @@ tickProto.getLabelSize = function () {
   else
     return _tickGetLabelSize.call(this);
 };
-
+//Recurses down the category tree to find its maximum depth
+function calculateDepth(categories){
+	var nxtCats = categories[0].categories;
+	return nxtCats ? (calculateDepth(nxtCats) + 1) : 0;
+};
 }(Highcharts, HighchartsAdapter));
