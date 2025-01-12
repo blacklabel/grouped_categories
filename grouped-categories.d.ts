12260aa66c366ca64751462bbdc7d91bcb9a358f
Michael Wind <michael.wind@outlook.at>
2025-01-12 20:31:48 +0100

    Add typescript definition

---
+++ grouped-categories.d.ts
@@ -0,0 +1,4 @@
+import * as _Highcharts from "highcharts/highcharts";
+export function factory(highcharts: typeof Highcharts): void;
+export default factory;
+export let Highcharts: typeof _Highcharts;
